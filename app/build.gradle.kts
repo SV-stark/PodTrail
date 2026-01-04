cab331cfea8dd5cd72dafbad2488b389fd074c6a
SV-Stark <Suyashsverma335@gmail.com>
2026-01-04 20:17:55 +0530

    Remove unused androidx.appcompat:appcompat dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ dependencies {
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+
+
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     // Jetpack Compose
