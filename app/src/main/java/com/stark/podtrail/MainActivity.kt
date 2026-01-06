11add229520245268d870842e7b5719d76fec44b
SV-Stark <Suyashsverma335@gmail.com>
2026-01-06 11:13:51 +0530

    Fix unresolved reference for nestedScroll

--- app/src/main/java/com/stark/podtrail/MainActivity.kt
+++ app/src/main/java/com/stark/podtrail/MainActivity.kt
@@ -3,6 +3,7 @@ package com.stark.podtrail
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
